65351ee3490b089ca92888618da0a563ada1d394
luofu <mqa010225@163.com>
2023-10-08 22:19:51 +0800

    some new exercises

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,14 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
-
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(nun: i32) -> i32 {
+    if nun <= 40 {
+        nun * 2
+    } else {
+        nun
+    }
+}
 
 // Don't modify this function!
 #[test]
